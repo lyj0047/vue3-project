52af58f1701316799ce7dbd71355b02f96a88bfd
yujeong.lee <lyj0047@naver.com>
2024-07-19 10:32:42 +0900

    pagenation 적용 전 코드

--- src/App.vue
+++ src/App.vue
@@ -3,39 +3,20 @@
     <h2>To-Do List</h2>
     <input
       class="form-control"
-      type="type"
+      type="text"
       v-model="searchText"
       placeholder="Search"
     />
     <hr />
     <TodoSimpleForm @add-todo="addTodo" />
     <div style="color: red">{{ error }}</div>
+
     <div v-if="!filteredTodos.length">There is nothing to display</div>
     <TodoList
       :todos="filteredTodos"
       @toggle-todo="toggleTodo"
       @delete-todo="deleteTodo"
     />
-    <hr />
-    <nav aria-label="Page navigation example">
-      <ul class="pagination">
-        <li class="page-item">
-          <a class="page-link" href="#"> Previous </a>
-        </li>
-        <li class="page-item">
-          <a class="page-link" href="#">1</a>
-        </li>
-        <li class="page-item">
-          <a class="page-link" href="#">2</a>
-        </li>
-        <li class="page-item">
-          <a class="page-link" href="#">3</a>
-        </li>
-        <li class="page-item">
-          <a class="page-link" href="#">Next</a>
-        </li>
-      </ul>
-    </nav>
   </div>
 </template>
 
@@ -56,26 +37,21 @@ export default {
 
     const getTodos = async () => {
       try {
-        const res = await axios.get(
-          "http://localhost:3000/todos?_page=1&_limit=5"
-        );
-        console.log(res.headers);
-        // comming soon
+        const res = await axios.get("http://localhost:3000/todos");
         todos.value = res.data;
       } catch (err) {
         console.log(err);
         error.value = "Something went wrong.";
       }
     };
+
     getTodos();
 
     const addTodo = async (todo) => {
+      // 데이터베이스 투두를 저장
       error.value = "";
-      // 데이터베이스 투두를 저장 <- axios를 이용하여 post request를 보냄
-      // 데이터 베이스 실행 명령어 : npx json-server db.json
       try {
         const res = await axios.post("http://localhost:3000/todos", {
-          // id는 자동으로 추가됨.
           subject: todo.subject,
           completed: todo.completed,
         });
@@ -84,7 +60,6 @@ export default {
         console.log(err);
         error.value = "Something went wrong.";
       }
-      console.log("hello");
     };
 
     const deleteTodo = async (index) => {
@@ -92,6 +67,7 @@ export default {
       const id = todos.value[index].id;
       try {
         await axios.delete("http://localhost:3000/todos/" + id);
+
         todos.value.splice(index, 1);
       } catch (err) {
         console.log(err);
@@ -106,11 +82,12 @@ export default {
         await axios.patch("http://localhost:3000/todos/" + id, {
           completed: !todos.value[index].completed,
         });
+
+        todos.value[index].completed = !todos.value[index].completed;
       } catch (err) {
         console.log(err);
         error.value = "Something went wrong.";
       }
-      todos.value[index].completed = !todos.value[index].completed;
     };
 
     const searchText = ref("");
@@ -120,17 +97,17 @@ export default {
           return todo.subject.includes(searchText.value);
         });
       }
+
       return todos.value;
     });
-
     return {
       todos,
-      error,
       addTodo,
-      toggleTodo,
       deleteTodo,
+      toggleTodo,
       searchText,
       filteredTodos,
+      error,
     };
   },
 };
@@ -138,9 +115,7 @@ export default {
 
 <style>
 .todo {
-  color: grey;
+  color: gray;
   text-decoration: line-through;
-
-  text-align: center;
 }
 </style>
