b32ce93afd79764c45c3c8bb3299507e4c9e317f
yujeong.lee <lyj0047@naver.com>
2024-07-22 10:38:42 +0900

    주석 수정

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    // a태그가 아닌 router-link를 사용하면 페이지 리로딩 없이 페이지를 이동할 수 있다.
+    <!--a태그가 아닌 router-link를 사용하면 페이지 리로딩 없이 페이지를 이동할 수 있다.-->
     <router-link class="navbar-brand" to="/">Lee's Todo List</router-link>
 
     <ul class="navbar-nav mr-auto">
